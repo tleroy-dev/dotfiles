ad28d316f12270c2fc8e1ca5dbadf336f082ae41
Theophile Leroy <perso@tleroy.me>
2020-05-08 05:17:17 +0200

    Modify bash config and add scripts

--- config.bash
+++ config.bash
@@ -1,6 +1,13 @@
-#Git related aliases
+set -o vi
+
+#Alias
+unalias -a
 alias save="/home/tleroy/Scripts/save"
 alias gaa="git add -A ."
 alias gc="git commit -m \"Working commit\""
 alias gp="git push -u origin"
+
 alias vi=vim
+
+# Add scripts folder to $PATH
+export PATH=$HOME/Repos/gitlab.com/tleroy/dotfiles/scripts:$PATH
