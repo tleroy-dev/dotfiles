8d6a8953a8e30a5780133c27655c71c8130fca59
Theophile Leroy <perso@tleroy.me>
2020-05-08 19:48:52 +0200

    add clear alias

--- config.bash
+++ config.bash
@@ -8,6 +8,7 @@ alias gc="git commit -m \"Working commit\""
 alias gp="git push -u origin"
 
 alias vi=vim
+alias c=clear
 
 # Add scripts folder to $PATH
 export PATH=$HOME/Repos/gitlab.com/tleroy/dotfiles/scripts:$PATH
